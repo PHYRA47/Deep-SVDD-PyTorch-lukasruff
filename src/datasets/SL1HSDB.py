94ea7d502dafbccab5b43a5a43a250a1feb7d12c
Fromsa Teshome Negasa <FromsaTeshome.Negasa@sony.com>
2025-05-05 18:54:32 +0200

    Enhance SL1HSDB class to support multiple subjects and random selection in reflectance loading

--- src/datasets/SL1HSDB.py
+++ src/datasets/SL1HSDB.py
@@ -7,7 +7,8 @@ from torch.utils.data import Dataset
 
 class SL1HSDB(Dataset):
     def __init__(self, 
-                 idx=None,
+                 num_subjects=None,
+                 randomize=True,
                  patch_size=32, 
                  interpolate=True, 
                  patches_per_file=1, 
@@ -27,8 +28,9 @@ class SL1HSDB(Dataset):
         self.patch_size = patch_size  # Single integer for square patch
         self.interpolate = interpolate
         self.patches_per_file = patches_per_file
+        self.num_subjects = num_subjects
+        self.randomize = randomize
         self.overlapping = overlapping
-        self.idx = idx
 
         self.verbose = verbose
 
@@ -39,46 +41,54 @@ class SL1HSDB(Dataset):
         if self.interpolate:
             self.target_wavelengths = np.linspace(400, 700, 31)
 
-    def _load_reflectance(self):
+    def _load_reflectance(self, num_subjects=1, randomize=True):
         """
-        Load reflectance data from the .mat file. If idx is not provided, randomly pick one.
+        Load reflectance data from the .mat files. If idx is not provided, randomly pick files.
 
         Args:
-            idx (int, optional): Index of the .mat file. If None, a random index is chosen.
+            num_subjects (int): Number of subjects to process.
+            randomize (bool): Whether to randomly select the files.
 
         Returns:
-            tuple: (np.ndarray, str)
-                - Reflectance data.
-                - Name of the picked file.
+            tuple: (list of np.ndarray, list of str)
+                - List of reflectance data arrays.
+                - List of corresponding file names.
         """
-        if self.idx is not None:
-            idx = self.idx
+        if randomize:
+            selected_indices = random.sample(range(len(self.mat_files)), num_subjects)
         else:
-            idx = random.randint(0, len(self.mat_files) - 1)
+            selected_indices = list(range(min(num_subjects, len(self.mat_files))))
 
-        file_name = self.mat_files[idx]
-        file_path = os.path.join(self.path, file_name)
-        mat_data = scipy.io.loadmat(file_path)
-        hsi_data = mat_data['HS_skin_data']
-        reflectance = hsi_data['reflectance'][0][0]
+        reflectance_list = []
+        file_names = []
 
-        if self.verbose:
-            pass # print(f"Loading {file_name} with shape {reflectance.shape}")
+        for idx in selected_indices:
+            file_name = self.mat_files[idx]
+            file_path = os.path.join(self.path, file_name)
+            mat_data = scipy.io.loadmat(file_path)
+            hsi_data = mat_data['HS_skin_data']
+            reflectance = hsi_data['reflectance'][0][0]
 
-        # Interpolate reflectance if required
-        if self.interpolate:
-            original_wavelengths = hsi_data['wavelengths'][0][0][0]
-            reflectance_interpolated = np.zeros((31, reflectance.shape[1], reflectance.shape[2]))
-            for i in range(reflectance.shape[1]):  # Iterate over rows
-                for j in range(reflectance.shape[2]):  # Iterate over columns
-                    reflectance_interpolated[:, i, j] = np.interp(
-                        self.target_wavelengths, original_wavelengths, reflectance[:, i, j]
-                    )
-            reflectance = reflectance_interpolated
+            if self.verbose:
+                pass  # print(f"Loading {file_name} with shape {reflectance.shape}")
 
-        return reflectance, file_name
+            # Interpolate reflectance if required
+            if self.interpolate:
+                original_wavelengths = hsi_data['wavelengths'][0][0][0]
+                reflectance_interpolated = np.zeros((31, reflectance.shape[1], reflectance.shape[2]))
+                for i in range(reflectance.shape[1]):  # Iterate over rows
+                    for j in range(reflectance.shape[2]):  # Iterate over columns
+                        reflectance_interpolated[:, i, j] = np.interp(
+                            self.target_wavelengths, original_wavelengths, reflectance[:, i, j]
+                        )
+                reflectance = reflectance_interpolated
 
-    def _generate_patches(self, idx):
+            reflectance_list.append(reflectance)
+            file_names.append(file_name)
+
+        return reflectance_list, file_names
+
+    def _generate_patches(self):
         """
         Generate patches from the reflectance data.
 
@@ -89,7 +99,7 @@ class SL1HSDB(Dataset):
             list of tuples: [(patch_tensor, label, global_idx), ...]
         """
         # Load reflectance data
-        reflectance, file_name = self._load_reflectance()
+        reflectance, file_name = self._load_reflectance(self.num_subjects, self.randomize)
 
         # Determine stride based on overlapping
         random_stride = self.patch_size // 2 if self.overlapping else self.patch_size
@@ -104,16 +114,16 @@ class SL1HSDB(Dataset):
             for j in range(0, w - self.patch_size + 1, random_stride)
         ]
 
-        # Shuffle positions to introduce randomness
-        random.shuffle(possible_positions)
+        # Randomly select one position
+        top, left = random.choice(possible_positions)
 
-        # Select up to patches_per_file positions
-        for i in range(min(self.patches_per_file, len(possible_positions))):
-            top, left = possible_positions[i]
-            patch = reflectance[:, top:top + self.patch_size, left:left + self.patch_size]
-            patches.append((torch.tensor(patch, dtype=torch.float32), 0, idx))
+        # Extract the patch
+        patch = reflectance[:, top:top + self.patch_size, left:left + self.patch_size]
 
-        return patches
+        # Convert the patch to a PyTorch tensor
+        patch_tensor = torch.tensor(patch, dtype=torch.float32)
+
+        return patch_tensor, file_name
 
     def __len__(self):
         return len(self.mat_files)
@@ -135,11 +145,18 @@ class SL1HSDB(Dataset):
         label = 0
 
         # Generate patches
-        patch_tensor = self._generate_patches(idx)
+        patch_tensor, file_name = self._generate_patches()
 
         if hasattr(self, 'global_offset'):
             global_idx = idx + self.global_offset
         else:
             global_idx = idx
+
+        if self.verbose:
+            print(f"Generating Patch".center(32, '-'))
+            print(f"{'From':<20}: {file_name:>10}")
+            print(f"{'Patch size':<20}: {f'{self.patch_size} x {self.patch_size}':>10}")
+            print(f"{'Label':<20}: {label:>10}")
+            print("-"*32)
 
         return patch_tensor, label, global_idx
